1dec410d8b4aac1e62450ee5caf9e6720733c6d3
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-03-21 19:38:47 -0500

    parseInt changes

--- super-disco-master/Develop/Day-Scheduler.js
+++ super-disco-master/Develop/Day-Scheduler.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     // listen for save button clicks
     $(".saveBtn").on("click", function(){
+        window.alert("Task saved")
         var value = $(this).siblings(".description").val();
         console.log(value);
         var time = $(this).parent().attr("id");
@@ -15,25 +16,32 @@ $(document).ready(function() {
 
     })
 
+    let timeOfDay = document.querySelector("time-block");
+    // while todayDate is current time: set to red 
+    
+    // if time id is not equal to 
+    
+    // parse hour values into hour integers
+    
+    // 
+    
+    let todayDate = moment(new Date()).format("LLLL");
+    let todayDate2 = moment().hours();
+    console.log(todayDate2);
+    
+    function loadDay(){  
+        $(timeOfDay).each()(function(){
+        
+        var currentHour = parseInt
+    }
+    )}
+    loadDay()
+    // var currentDay = newDate(){
+    
+    // }
+    document.querySelector("#currentDay").innerHTML = todayDate
+    // $(m)
+    // document.querySelector('h2.some-class').innerText = document.querySelector('div.some-class').innerText;
+    
+    localStorage
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var currentDay = newDate(){
-let todayDate = moment(new Date()).format("MM/DD/YYYY");
-// }
-document.querySelector("#currentDay").innerHTML = todayDate
-// $(m)
-// document.querySelector('h2.some-class').innerText = document.querySelector('div.some-class').innerText;
-
